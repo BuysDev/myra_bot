d6c4c59694a8fc272b5e4a9e08b27045f76ddb1f
Guilherme Buys <guibuys55@gmail.com>
2026-01-22 23:39:15 -0300

    fix: removing unecessary things

--- src/functions/user.ts
+++
@@ -1,7 +0,0 @@
-import { UserDocument } from "../discord/database/documents/UserDocuments.js";
-import { DeepPartial } from "../discord/settings/@types/utils.js";
-
-type PartialUserDocument = DeepPartial<UserDocument>;
-
-export async function updateUserData(id: string, data: PartialUserDocument): Promise<void> {
-}
